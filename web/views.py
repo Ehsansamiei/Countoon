6f27d1ab4c639aac27b3ad7760fb8edc7a3ad94d
Ehsansamiei <ehsan.samiei84@gmail.com>
2025-10-18 22:00:03 +0330

    post api for expense is working

--- web/views.py
+++ web/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
+
+
+@csrf_exempt
 def submit_expense(request):
     """ user submits an expense"""
     print(request.POST)
-
+    print("method:", request.method)
+    print("GET data:", request.GET)
+    print("POST data:", request.POST)
     return JsonResponse({
         'status' : 'ok'
-    })
+    })  
